7ad51375bdc321781188f7ca89fd31c6c755e5af
Eric Cacciatori Bellini <eric.cacciatori@hotmail.com>
2022-08-25 18:09:25 -0300

    estrutura de responsividade criada

---
+++ social_media/lib/utils/cores.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const corPrincipal = Color.fromARGB(255, 234, 234, 234);
+const corSecundaria = Color.fromARGB(255, 200, 184, 5);
+const corBackground = Color.fromARGB(255, 34, 32, 32);
