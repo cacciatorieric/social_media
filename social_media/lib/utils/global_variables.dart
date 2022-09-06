bd407eadf656d0d83e45babb9ff31b5cafa62c05
Eric Cacciatori Bellini <eric.cacciatori@hotmail.com>
2022-09-06 16:49:03 -0300

    pesquisa de usuarios

--- social_media/lib/utils/global_variables.dart
+++ social_media/lib/utils/global_variables.dart
@@ -2,26 +2,14 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media/pages/screens/add_post_screen.dart';
 import 'package:social_media/pages/screens/feed_screen.dart';
+import 'package:social_media/pages/screens/search_screen.dart';
 import 'package:uuid/uuid.dart';
 
 const webScreenSize = 600;
 
 var homeScreenItens = [
   const FeedScreen(),
-  kIsWeb
-      ? Container(
-          color: Colors.blueAccent,
-          child: const Center(child: Text('Search Web')),
-        )
-      : Container(
-          color: Colors.greenAccent,
-          child: const Center(
-            child: Text(
-              'Search Mobile',
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-        ),
+  const SearchScreen(),
   const AddPostScreen(),
   kIsWeb
       ? Container(
